bbcfcbc793f329f4fc675e941aab8e23b3ecd5f3
DejanZerajic <dzerajik@hotmail.com>
2024-12-03 15:01:34 +0100

    Add files via upload

---
+++ Class01Exercises/script.js
@@ -0,0 +1,41 @@
+let minuteS = 36; //definiram varijabla za minuti, no ne znam kako da ja definiram za da prasa kolku minuti
+let secondS = 60; //definiram constanta
+let resulT = minuteS * secondS; //postavuvam ravenka za presmetuvanje minuti vo sekundi
+console.log ("Prva zadaca: Vo " + minuteS + " minuti ima " + resulT + " sekundi") //go pokazuvam rezultatot vo console
+
+console.log ("==========================================================")
+
+let number1 = 385; //postavuvam eden broj
+number1 += 1; //assigment operator za da ja smenam varijablata za +1
+console.log ("Vtora zadaca: Zgolemenata vrednost za 1 na brojot 385 iznesuva " + number1) // go pokazuvam rezultatot vo console
+
+console.log ("==========================================================")
+
+let triangleHeight = 5; //definiram visina na triagolnik
+let triangleBase = 10; //definiram osnova na triagolnik
+let areaT = triangleBase * triangleHeight / 2; //ja postavuvam ravenkata, nema potreba od zagradi, se izvrsuva od levo kon desno
+console.log ("Treta zadaca: Povrsinata na ovoj triagiolnik iznesuva " + areaT + " kvadratni santimetri." ) //rezultatot vo console
+
+console.log ("==========================================================")
+
+let rectangleLenght = 15; //definiram dolzina na pravoagolnik
+let rectangleWidth = 35; //definiram sirina na pravoagolnik
+let rectanglePerimeter = 2 * (rectangleLenght + rectangleWidth); //ja postavuvam ravenkata, 
+console.log ("Cetvrta zadaca: Perimetarot na ovoj pravoagolnik iznesuva " + rectanglePerimeter + " kvadratni santimetri." ) //rezultatot vo console
+
+console.log ("==========================================================")
+
+let pi = 3.1415; //definiram pi 
+let radiusC = 13; //radiusot na krugot
+let areaC = pi * (radiusC * radiusC); //formulata 
+console.log ("Peta zadaca: Povrsinata na ovoj krug iznesuva " + areaC + " kvadratni santimetri." ) //rezultatot vo console
+
+
+
+
+
+
+
+
+
+
